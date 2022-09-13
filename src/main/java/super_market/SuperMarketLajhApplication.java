34ab64893663b7f60687b542d9ddb38fb98e0697
Jeffer <jefferd2016@gmail.com>
2022-09-13 15:41:37 -0500

    Initial commit

---
+++ src/main/java/super_market/SuperMarketLajhApplication.java
@@ -0,0 +1,13 @@
+package super_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMarketLajhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMarketLajhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/super_market/SuperMarketLajhApplicationTests.java
@@ -0,0 +1,13 @@
+package super_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketLajhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
