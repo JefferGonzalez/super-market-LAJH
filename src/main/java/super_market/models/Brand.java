d15a93607a2d32f780757366de9d282b7eedafcc
JefferGonzalez <jefferd2016@gmail.com>
2022-10-04 15:13:26 -0500

    Add relationships in Brand entity

--- src/main/java/super_market/models/Brand.java
+++ src/main/java/super_market/models/Brand.java
@@ -1,24 +1,28 @@
 package super_market.models;
+
+import java.util.List;
+
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "brand")
 public class Brand {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
 
-    public Brand() {
-        
-    }
+    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "brand_id")
+    @JsonIgnoreProperties("brand")
+    private List<Product> product;
 
-    public Brand(Integer id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-    
 }
