16e221e5d41ef1a762ff5254f40695e78fd3bac8
JefferGonzalez <jefferd2016@gmail.com>
2022-10-04 15:15:01 -0500

    Add relationships in Purchase Detail entity

--- src/main/java/super_market/models/PurchaseDetail.java
+++ src/main/java/super_market/models/PurchaseDetail.java
@@ -1,24 +1,32 @@
 package super_market.models;
+
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "purchase_detail")
 public class PurchaseDetail {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer quantity;
     private Double price;
-    
-    public PurchaseDetail() {
-        
-    }
 
-    public PurchaseDetail(Integer id, Integer quantity, Double price) {
-        this.id = id;
-        this.quantity = quantity;
-        this.price = price;
-    }
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @JsonIgnoreProperties("purchase_detail")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "purchase_invoice_id")
+    @JsonIgnoreProperties("purchase_detail")
+    private PurchaseInvoice purchase_invoice;
 
 }
